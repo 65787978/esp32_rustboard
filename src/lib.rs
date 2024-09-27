50751e7b6dc15bb08ceab57e1925f604516f7b65
Nederg <ergxned@proton.me>
2024-09-27 20:24:39 +0200

    fixed couple of things

--- src/lib.rs
+++ src/lib.rs
@@ -2,32 +2,12 @@
 *********************************************************************************************
 BASE LAYER:
 
-PINS|  2  |  3  |  10 |  6  |  7  |  4  |           PINS|  2  |  3  |  10 |  6  |  7  |  11 |
-  0 |_ESC_|__1__|__2__|__3__|__4__|__5__|             0 |__6__|__7__|__8__|__9__|__0__|__-__|
-  1 |_TAB_|__q__|__w__|__e__|__r__|__t__|             1 |__y__|__u__|__i__|__o__|__p__|__[__|
- 12 |_BSP_|__a__|__s__|__d__|__f__|__g__|             12|__h__|__j__|__k__|__l__|__;__|__]__|
- 18 |_LYR_|__z__|__x__|__c__|__v__|__b__|             18|__n__|__m__|__,__|__.__|__/__|__\__|
- 19 |_____|_____|_____|_CTL_|_SFT_|_SPC_|             19|_ENT_|_CTL_|_LYR_|_____|_____|_____|
-
-*********************************************************************************************
-SHIFT LAYER:
-
-PINS|  2  |  3  |  10 |  6  |  7  |  4  |           PINS|  2  |  3  |  10 |  6  |  7  |  11 |
-  0 |_ESC_|__!__|__@__|__#__|__$__|__%__|             0 |__^__|__&__|__*__|__(__|__)__|_ _ _|
-  1 |_TAB_|__Q__|__W__|__E__|__R__|__T__|             1 |__Y__|__U__|__I__|__O__|__P__|__{__|
- 12 |_CAP_|__A__|__S__|__D__|__F__|__G__|             12|__H__|__J__|__K__|__L__|__:__|__}__|
- 18 |_____|__Z__|__X__|__C__|__V__|__B__|             18|__N__|__M__|__<__|__>__|__?__|__|__|
- 19 |_____|_____|_LYR_|_CTL_|_SFT_|_ENT_|             19|_SPC_|_BSP_|_CTL_|_LYR_|_____|_____|
-
- *********************************************************************************************
-UPPER LAYER:
-
-PINS|  2  |  3  |  10 |  6  |  7  |  4  |           PINS|  2  |  3  |  10 |  6  |  7  |  11 |
-  0 |_ESC_|_____|_____|_____|_____|_____|             0 |_____|_____|_____|_____|_____|_____|
-  1 |_TAB_|_____|__UP_|_____|_____|_____|             1 |_____|_____|_____|_____|_____|_____|
- 12 |_CAP_|_LFT_|_DWN_|_RGT_|_____|_____|             12|_____|_____|_____|_____|_____|_____|
- 18 |_____|_____|_____|_____|_____|_____|             18|_____|_____|_____|_____|_____|_____|
- 19 |_____|_____|_LYR_|_CTL_|_SFT_|_ENT_|             19|_SPC_|_BSP_|_CTL_|_LYR_|_____|_____|
+X \ Y|  0  |  1  |  2  |  3  |  4  |  5  |           X \ Y|  0  |  1  |  2  |  3  |  4  |  5 |
+   0 |_ESC_|__1__|__2__|__3__|__4__|__5__|              0 |__6__|__7__|__8__|__9__|__0__|__-__|
+   1 |_TAB_|__q__|__w__|__e__|__r__|__t__|              1 |__y__|__u__|__i__|__o__|__p__|__[__|
+   2 |_BSP_|__a__|__s__|__d__|__f__|__g__|              2 |__h__|__j__|__k__|__l__|__;__|__]__|
+   3 |_LYR_|__z__|__x__|__c__|__v__|__b__|              3 |__n__|__m__|__,__|__.__|__/__|__\__|
+   4 |_____|_____|_____|_CTL_|_SFT_|_SPC_|              4 |_ENT_|_CTL_|_LYR_|_____|_____|_____|
 
 */
 use crate::enums::*;
@@ -38,31 +18,43 @@ use std::collections::HashMap;
 pub mod ble_keyboard;
 pub mod enums;
 
+pub mod delay {
+    use embassy_time::{Duration, Instant};
+
+    pub fn delay_us(us: u64) {
+        let duration = Duration::from_micros(us);
+        let end_time = Instant::now() + duration;
+
+        while Instant::now() < end_time {}
+    }
+}
+
 pub const ROWS: usize = 5;
 pub const COLS: usize = 6;
+pub const LAYER_KEY: (i8, i8) = (3, 0);
 
 pub struct KeyMatrix<'a> {
     pub rows: [PinDriver<'a, AnyOutputPin, Output>; ROWS],
     pub cols: [PinDriver<'a, AnyInputPin, Input>; COLS],
 }
 
-pub struct Layers {
-    pub base_layer: HashMap<(i32, i32), HidMapings>,
+pub enum Layer {
+    Base,
+    Upper,
 }
 
-pub struct KeyboardSide<'a> {
-    pub layer: Layers,
+pub struct KeyboardLeftSide<'a> {
+    pub base_layer: HashMap<(i8, i8), HidMapings>,
     pub key_matrix: KeyMatrix<'a>,
+    pub layer: Layer,
 }
 
-impl KeyboardSide<'_> {
-    pub fn new() -> KeyboardSide<'static> {
+impl KeyboardLeftSide<'_> {
+    pub fn new() -> KeyboardLeftSide<'static> {
         let peripherals = Peripherals::take().unwrap();
 
-        KeyboardSide {
-            layer: Layers {
-                base_layer: HashMap::new(),
-            },
+        KeyboardLeftSide {
+            base_layer: HashMap::new(),
             key_matrix: KeyMatrix {
                 rows: [
                     PinDriver::output(peripherals.pins.gpio0.downgrade_output()).unwrap(),
@@ -80,43 +72,44 @@ impl KeyboardSide<'_> {
                     PinDriver::input(peripherals.pins.gpio4.downgrade_input()).unwrap(),
                 ],
             },
+            layer: Layer::Base,
         }
     }
 
     pub fn initialize_base_layer(&mut self) {
-        self.base_layer.insert((0, 2), HidMapings::Escape); // ESC
-        self.base_layer.insert((0, 3), HidMapings::Num1); // 1
-        self.base_layer.insert((0, 10), HidMapings::Num2); // 2
-        self.base_layer.insert((0, 6), HidMapings::Num3); // 3
-        self.base_layer.insert((0, 7), HidMapings::Num4); // 4
-        self.base_layer.insert((0, 4), HidMapings::Num5); // 5
+        self.base_layer.insert((0, 0), HidMapings::Escape); // ESC
+        self.base_layer.insert((0, 1), HidMapings::Num1); // 1
+        self.base_layer.insert((0, 2), HidMapings::Num2); // 2
+        self.base_layer.insert((0, 3), HidMapings::Num3); // 3
+        self.base_layer.insert((0, 4), HidMapings::Num4); // 4
+        self.base_layer.insert((0, 5), HidMapings::Num5); // 5
 
-        self.base_layer.insert((1, 2), HidMapings::Tab); // TAB
-        self.base_layer.insert((1, 3), HidMapings::Q); // q
-        self.base_layer.insert((1, 10), HidMapings::W); // w
-        self.base_layer.insert((1, 6), HidMapings::E); // e
-        self.base_layer.insert((1, 7), HidMapings::R); // r
-        self.base_layer.insert((1, 4), HidMapings::T); // t
+        self.base_layer.insert((1, 0), HidMapings::Tab); // TAB
+        self.base_layer.insert((1, 1), HidMapings::Q); // q
+        self.base_layer.insert((1, 2), HidMapings::W); // w
+        self.base_layer.insert((1, 3), HidMapings::E); // e
+        self.base_layer.insert((1, 4), HidMapings::R); // r
+        self.base_layer.insert((1, 5), HidMapings::T); // t
 
-        self.base_layer.insert((12, 2), HidMapings::Capslock); // BACKSPACE
-        self.base_layer.insert((12, 3), HidMapings::A); // a
-        self.base_layer.insert((12, 10), HidMapings::S); // s
-        self.base_layer.insert((12, 6), HidMapings::D); // d
-        self.base_layer.insert((12, 7), HidMapings::F); // f
-        self.base_layer.insert((12, 4), HidMapings::G); // g
+        self.base_layer.insert((2, 0), HidMapings::Capslock); // BACKSPACE
+        self.base_layer.insert((2, 1), HidMapings::A); // a
+        self.base_layer.insert((2, 2), HidMapings::S); // s
+        self.base_layer.insert((2, 3), HidMapings::D); // d
+        self.base_layer.insert((2, 4), HidMapings::F); // f
+        self.base_layer.insert((2, 5), HidMapings::G); // g
 
-        self.base_layer.insert((18, 2), HidMapings::None); // LAYER
-        self.base_layer.insert((18, 3), HidMapings::Z); // z
-        self.base_layer.insert((18, 10), HidMapings::X); // x
-        self.base_layer.insert((18, 6), HidMapings::C); // c
-        self.base_layer.insert((18, 7), HidMapings::V); // v
-        self.base_layer.insert((18, 4), HidMapings::B); // b
+        self.base_layer.insert((3, 0), HidMapings::None); // LAYER
+        self.base_layer.insert((3, 1), HidMapings::Z); // z
+        self.base_layer.insert((3, 2), HidMapings::X); // x
+        self.base_layer.insert((3, 3), HidMapings::C); // c
+        self.base_layer.insert((3, 4), HidMapings::V); // v
+        self.base_layer.insert((3, 5), HidMapings::B); // b
 
-        self.base_layer.insert((19, 2), HidMapings::None); //
-        self.base_layer.insert((19, 3), HidMapings::None); //
-        self.base_layer.insert((19, 10), HidMapings::None); //
-        self.base_layer.insert((19, 6), HidMapings::Control); // CONTROL
-        self.base_layer.insert((19, 7), HidMapings::Shift); // SHIFT
-        self.base_layer.insert((19, 4), HidMapings::Space); // SPACE
+        self.base_layer.insert((4, 0), HidMapings::None); //
+        self.base_layer.insert((4, 1), HidMapings::None); //
+        self.base_layer.insert((4, 2), HidMapings::None); //
+        self.base_layer.insert((4, 3), HidMapings::Control); // CONTROL
+        self.base_layer.insert((4, 4), HidMapings::Shift); // SHIFT
+        self.base_layer.insert((4, 5), HidMapings::Space); // SPACE
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ use crate::ble_keyboard::*;
 use anyhow;
 use enums::HidMapings;
 use esp32_rust_split_keyboard::*;
-use esp_idf_svc::hal::delay::FreeRtos;
 
 fn main() -> anyhow::Result<()> {
     esp_idf_svc::sys::link_patches();
@@ -22,7 +21,7 @@ fn main() -> anyhow::Result<()> {
     log::info!("BLE Initialized...");
 
     /* initialize matrix */
-    let mut keyboard = KeyboardSide::new();
+    let mut keyboard = KeyboardLeftSide::new();
     keyboard.initialize_base_layer();
 
     log::info!("Keyboard Initialized...");
@@ -30,65 +29,114 @@ fn main() -> anyhow::Result<()> {
     /* initialize bool matrix */
     let mut bool_matrix: [[bool; COLS]; ROWS] = [[false; COLS]; ROWS];
 
-    /* Run the tasks in parallel */
+    /* Run the main loop */
     loop {
         if ble_connection.connected() {
-            poll_matrix(&mut keyboard.key_matrix, &mut bool_matrix);
+            poll_matrix(&mut keyboard.key_matrix, &mut bool_matrix).unwrap();
+
+            /* */
+            let keys = get_keys_pressed(&mut bool_matrix);
+
+            if !keys.is_empty() {
+                for key in keys {
+                    if key == LAYER_KEY {
+                        match keyboard.layer {
+                            Layer::Base => keyboard.layer = Layer::Upper,
+                            Layer::Upper => keyboard.layer = Layer::Base,
+                        }
+                    }
+
+                    match keyboard.layer {
+                        Layer::Base => {
+                            if let Some(valid_key) = keyboard.base_layer.get(&key) {
+                                // if *valid_key ==
+                                let modifier = HidMapings::None;
+
+                                ble_connection.press(*valid_key, modifier);
+                                ble_connection.release();
+                            }
+                        }
+                        Layer::Upper => {
+                            todo!("Not yet implemented...");
+                        }
+                    }
+                }
+            }
+            delay::delay_us(100);
+        } else {
+            log::info!("Keyboard not connected!");
+            delay::delay_us(100000);
         }
+    }
+}
+
+fn poll_matrix(
+    matrix: &mut KeyMatrix<'_>,
+    bool_matrix: &mut [[bool; COLS]; ROWS],
+) -> anyhow::Result<()> {
+    /* initialize counts */
+    let mut row_count: usize = 0;
+    let mut col_count: usize = 0;
+
+    /* check rows and cols */
+    for row in matrix.rows.iter_mut() {
+        /* set row to high */
+        row.set_high().unwrap();
+
+        /* delay so pin can propagate */
+        delay::delay_us(10);
+
+        /* check if a col is high */
+        for col in matrix.cols.iter_mut() {
+            /* if a col is high */
+            if col.is_high() {
+                /* store pressed keys */
+                bool_matrix[row_count][col_count] = true;
+            }
+            /* increment col */
+            col_count += 1;
+        }
+
+        /* set row to low */
+        row.set_low().unwrap();
+
+        /* increment row */
+        row_count += 1;
+
+        /* reset col count */
+        col_count = 0;
     }
 
     Ok(())
 }
 
-fn ble_transmit() -> ! {
-    loop {
-        /* check if connected */
-        if keyboard.connected() {
-            /* wait 10 ms */
-            FreeRtos::delay_ms(1);
+fn get_keys_pressed(bool_matrix: &mut [[bool; COLS]; ROWS]) -> Vec<(i8, i8)> {
+    /* initialize variables */
+    let mut keys_pressed: Vec<(i8, i8)> = Vec::new();
+    let (mut row_count, mut col_count): (i8, i8) = (0, 0);
 
-            /* send press key */
-            keyboard.press(key, modifier);
-            keyboard.release();
-        }
-
-        /* Delay so wdt doesn't kick in */
-        FreeRtos::delay_ms(1);
-    }
-}
-fn poll_matrix(matrix: &mut KeyMatrix<'_>, bool_matrix: &mut [[bool; COLS]; ROWS]) -> ! {
-    let mut arr_row: usize;
-    let mut arr_col: usize;
-
-    loop {
-        /* reset variables */
-        arr_row = 0;
-        arr_col = 0;
-
-        /* check rows and cols */
-        for row in matrix.rows.iter_mut() {
-            /* set row to high */
-            row.set_high().unwrap();
-
-            /* delay so pin can propagate */
-            FreeRtos::delay_ms(1);
-
-            /* check if a col is high */
-            for col in matrix.cols.iter_mut() {
-                /* if a col is high */
-                if col.is_high() {
-                    /* store pressed keys */
-                    bool_matrix[arr_col][arr_row] = true;
-                }
-                /* increment col */
-                arr_col += 1;
+    /* itter the matrix */
+    for row in bool_matrix {
+        /* itter col */
+        for col in row {
+            /* if an element is true */
+            if *col {
+                /* store it */
+                keys_pressed.push((row_count, col_count));
+                *col = false;
             }
 
-            /* set row to low */
-            row.set_low().unwrap();
-
-            /* increment row */
-            arr_row += 1;
+            /* increment col count */
+            col_count += 1;
         }
+
+        /* increment row count */
+        row_count += 1;
+
+        /* reset col count */
+        col_count = 0;
     }
+
+    /* return pressed keys */
+    keys_pressed
 }
