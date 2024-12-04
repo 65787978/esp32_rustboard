c9c8f1441fb80953bb1d80924667d26de87a4d13
Nederg <ergxned@proton.me>
2024-12-04 19:25:42 +0100

    debounce: added diferent debounce for the layer key

--- src/debounce/mod.rs
+++ src/debounce/mod.rs
@@ -1,9 +1,9 @@
 use crate::{
-    config::config::{DEBOUNCE_DELAY, PRESSED_KEYS_INDEXMAP_SIZE},
+    config::config::{DEBOUNCE_DELAY, DEBOUNCE_DELAY_LAYER_KEY,  PRESSED_KEYS_INDEXMAP_SIZE, LAYER_KEY},
     delay::delay_us,
     matrix::Key,
 };
-use embassy_time::Instant;
+use embassy_time::{Duration, Instant};
 use heapless::FnvIndexMap;
 use spin::mutex::Mutex;
 
@@ -23,9 +23,14 @@ pub async fn calculate_debounce(
         /* try to get a lock on keys_pressed */
         if let Some(mut keys_pressed) = keys_pressed.try_lock() {
             /* itter throught the pressed keys */
-            for (_key, debounce) in keys_pressed.iter_mut() {
+            for (key, debounce) in keys_pressed.iter_mut() {
+                let debounce_delay: Duration = match *key {
+                    LAYER_KEY => DEBOUNCE_DELAY_LAYER_KEY,
+                    _ =>  DEBOUNCE_DELAY,
+                };
+
                 /* check if the key has passed the debounce delay or has been released */
-                if Instant::now() >= debounce.key_pressed_time + DEBOUNCE_DELAY {
+                if Instant::now() >= debounce.key_pressed_time + debounce_delay {
                     debounce.key_state = KEY_RELEASED;
                 }
             }
