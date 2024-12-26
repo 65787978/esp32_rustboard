f7c0c01e98685badda6f19ddd1888834527cd7cf
Nederg <ergxned@proton.me>
2024-12-26 20:55:11 +0100

    config: adjusted debounce time, sleep delay and hashmap size

--- src/config/config.rs
+++ src/config/config.rs
@@ -16,11 +16,11 @@ use crate::matrix::Key;
 /* USER CONFIGURABLE PARAMETERS */
 pub const ROWS: usize = 4;
 pub const COLS: usize = 6;
-pub const DEBOUNCE_DELAY: Duration = Duration::from_millis(40);
+pub const DEBOUNCE_DELAY: Duration = Duration::from_millis(50);
 pub const DEBOUNCE_DELAY_LAYER_KEY: Duration = Duration::from_millis(300);
-pub const SLEEP_DELAY: Duration = Duration::from_millis(600000);
-pub const SLEEP_DELAY_INIT: Duration = Duration::from_millis(600000);
-pub const PRESSED_KEYS_INDEXMAP_SIZE: usize = 8;
+pub const SLEEP_DELAY: Duration = Duration::from_millis(100000);
+pub const SLEEP_DELAY_INIT: Duration = Duration::from_millis(60000);
+pub const PRESSED_KEYS_INDEXMAP_SIZE: usize = 16;
 pub const LAYER_INDEXMAP_SIZE: usize = 32;
 pub const ESP_POWER_LEVEL: EspPowerLevel = EspPowerLevel::Positive3;
 
